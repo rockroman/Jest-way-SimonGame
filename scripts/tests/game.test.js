a565e0aec4a34041ce50fe9a77065ac115423d44
rockroman <rock.rakic@gmail.com>
2022-11-04 00:47:45 +0000

    Add boiler plate for test file

--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -1,3 +1,13 @@
+const { beforeAll } = require("@jest/globals");
+
 /**
  * @jest-environment jsdom
  */
+beforeAll(()=>{
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html","utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+
+})
